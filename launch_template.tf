2a2da92a9c1fc35d76015fd500e7ac560fa2636d
shaikfirose <shaikfirose2002@gmail.com>
2024-11-15 11:15:31 +0530

    Create launch_template.tf

---
+++ launch_template.tf
@@ -0,0 +1,22 @@
+# launch_template.tf
+
+# Launch Template for Auto Scaling Group
+resource "aws_launch_template" "app_lt" {
+  name_prefix   = "app-"
+  image_id      = "ami-0aebec83a182ea7ea"  # Replace with your preferred Amazon Linux AMI
+  instance_type = "t2.micro"
+
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    echo "<h1>Hello, World</h1>" > /var/www/html/index.html
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+  EOF
+  )
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.web_sg.id]
+  }
+}
