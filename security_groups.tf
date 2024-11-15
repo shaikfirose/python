0592a187ac6470d0f96cdbc1388c8fea987603c6
shaikfirose <shaikfirose2002@gmail.com>
2024-11-15 11:14:41 +0530

    Create security_groups.tf

---
+++ security_groups.tf
@@ -0,0 +1,20 @@
+# security_groups.tf
+
+# Security Group for ALB and EC2
+resource "aws_security_group" "web_sg" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
